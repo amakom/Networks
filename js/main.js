44e35462941c68b85ef1f901c8008cb3820b3f63
amakom <obiajuluamakom@gmail.com>
2022-03-24 07:06:25 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
      $(".nav-link").on("click", function(){
          $(".navbar-collapse").collapse("hide");
      });
-     
-    /*------------------ Video Popup ---------------------- */
+    
+     /*------------------ Video Popup ---------------------- */
     const videoSrc = $("#player-1").attr("src");
     $(".video-play-btn, .video-popup").on("click", function(){
         if($(".video-popup").hasClass("open")){
@@ -31,3 +31,42 @@ $(document).ready(function() {
         }
     });
 });
+
+
+     
+    
+    /*------------------ Model Popup ---------------------- */
+
+    var model = document.getElementById('simpleModel');
+    var modelBtn = document.getElementById('modelBtn');
+    var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+    //Listen for open click
+    modelBtn.addEventListener('click', openModel);
+
+    //Listen for close click
+    closeBtn.addEventListener('click', closeModel);
+
+    //Listen for Outside click
+    window.addEventListener('click', outsideClick);
+
+
+    //function to open model
+    function openModel(){
+        model.style.display = 'block';
+    }
+
+
+    //function to close model
+    function closeModel(){
+        model.style.display = 'none';
+    }
+
+    //function to close model if outside click
+    function outsideClick(e){
+        if(e.target == model){
+            model.style.display = 'none';
+        }
+    }
+
+    
